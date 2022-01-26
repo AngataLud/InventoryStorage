9e72441df72aec6ae170eb1163beb60efefb2988
Ангел Коларов <angelkolarov17a@gmail.com>
2022-01-26 22:29:51 +0200

    sdf

---
+++ InventoryStorage/Models/ItemHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryStorage.Models
+{
+    public class ItemHistory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Item name")]
+        public string ItemName { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int OldLocation { get; set; }
+        [Required]
+        public int DateTime { get; set; }
+    }
+}
